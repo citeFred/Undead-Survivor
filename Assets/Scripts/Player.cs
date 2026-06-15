a99e82c050da48643ce30a5be18e183b22cd9a38
citefred <citefred@gmail.com>
2026-06-15 12:46:53 +0900

    feat: 에셋 임포트 및 플레이어 기본 이동 로직 구현

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    // 방향 키 입력 받는 벡터 ( x= 좌우, y= 상하)
+    public Vector2 inputVec;
+    
+    // 이동 속도
+    public float speed;
+    
+    // 객체가 가진 물리 컴포넌트
+    Rigidbody2D rigid;
+    // 스프라이트 렌더러 컴포넌트
+    SpriteRenderer spriter;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+    }
+    
+    // FixedUpdate: 물리 이동은 이곳에서
+    private void FixedUpdate()
+    {
+        // 다음에 이동할 양 = 방향 * 속도 * 프레임 시간
+        Vector2 nextVec = inputVec.normalized * (speed * Time.fixedDeltaTime);
+        // 현재위치+이동량 
+        rigid.MovePosition(rigid.position + nextVec);
+    }
+    
+    // LateUpdate: 모든 업데이트가 끝난 뒤 실행. 방향 반전같은 것은 후처리에 적합
+    void LateUpdate()
+    {
+        if (inputVec.x != 0)
+        {
+            spriter.flipX = inputVec.x < 0;
+        }
+    }
+
+    void OnMove(InputValue value)
+    {
+        inputVec = value.Get<Vector2>();
+    }
+    
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 300, 20), inputVec.ToString());
+    }
+}
