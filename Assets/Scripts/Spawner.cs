499dbdc53f276a1f0895ffe70050f78ad869a134
citefred <citefred@gmail.com>
2026-06-19 11:56:35 +0900

    feat: Enemy 적 자동 생성을 위한 스포너(like Factory) 구현

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -2,15 +2,37 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    // 적 생성 위치들 (스포너+스폰포인트들)
+    public Transform[] spawnPoint;
+    // 누적 시간 변수
+    private float timer;
 
-    // Update is called once per frame
+    void Awake()
+    {
+        // 스포너 자신+자식 트랜스폼을 한번에 가져옴
+        // GetComponentsInChildren 's' 복수형 이어야 자식 전부 가져옴 
+        spawnPoint = GetComponentsInChildren<Transform>();
+    }
+    
     void Update()
     {
+        // 매 프레임마다 경과 시간 누적
+        timer += Time.deltaTime;
         
+        // 0.2초마다 스폰 호출
+        if (timer > 0.2f)
+        {
+            timer = 0;
+            Spawn();
+        }
+    }
+
+    void Spawn()
+    {
+        // 풀에서 적을 꺼냄
+        GameObject enemy = GameManager.instance.pool.Get(0);
+        
+        // 스폰 포인트 중 하나에 배치 (랜덤으로, point들에서만 시작되도록 0제외)
+        enemy.transform.position = spawnPoint[Random.Range(1, spawnPoint.Length)].position;
     }
 }
