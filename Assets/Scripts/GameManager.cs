65f70330284c3b071c85c6dd88a2b912f6d0c7a6
citefred <citefred@gmail.com>
2026-06-22 11:12:42 +0900

    feat: GameManager 게임 시간 관리(난이도 관리용)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,9 +5,25 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;
     public Player player;
     public PoolManager pool;
-
+    // 흐르는 게임 시간 - 난이도 계산용도
+    public float gameTime;
+    // 최대 게임 시간 - 난이도 증가 기준
+    public float maxGameTime = 10f;
+    
     private void Awake()
     {
         instance = this;
+    }
+
+    void Update()
+    {
+        // 매 프레임마다 실제 흐른 시간을 누적
+        gameTime += Time.deltaTime;
+
+        // 최대 시간을 넘지 않도록 고정 (게임 종료 등 처리에 활용)
+        if (gameTime > maxGameTime)
+        {
+            gameTime = maxGameTime;
+        }
     }
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -30,7 +30,7 @@ public class Spawner : MonoBehaviour
     void Spawn()
     {
         // 풀에서 적을 꺼냄
-        GameObject enemy = GameManager.instance.pool.Get(0);
+        GameObject enemy = GameManager.instance.pool.Get(Random.Range(0, GameManager.instance.pool.prefabs.Length));
         
         // 스폰 포인트 중 하나에 배치 (랜덤으로, point들에서만 시작되도록 0제외)
         enemy.transform.position = spawnPoint[Random.Range(1, spawnPoint.Length)].position;
