245ca16fc33e82a5f226800c99f63d22548444f4
citefred <citefred@gmail.com>
2026-06-22 11:42:17 +0900

    feat: Spawner 시간 기반 난이도 데이터 활용 몬스터 스폰 설정(SpawnData 연동)

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,8 +4,13 @@ public class Spawner : MonoBehaviour
 {
     // 적 생성 위치들 (스포너+스폰포인트들)
     public Transform[] spawnPoint;
+    // 난이도별 스폰 설정 데이터
+    public SpawnData[] spawnData;
+    
     // 누적 시간 변수
     private float timer;
+    // 현재 난이도 (게임 시간에 따라 변함)
+    private int difficulty;
 
     void Awake()
     {
@@ -19,8 +24,15 @@ public class Spawner : MonoBehaviour
         // 매 프레임마다 경과 시간 누적
         timer += Time.deltaTime;
         
-        // 0.2초마다 스폰 호출
-        if (timer > 0.2f)
+        // 난이도 상승 연산
+        difficulty = (int)(GameManager.instance.gameTime / 10f);
+        
+        // 난이도가 데이터 개수를 넘지않도록 제한(배열 범위를 초과 방지)
+        if (difficulty >= spawnData.Length)
+            difficulty = spawnData.Length - 1;
+        
+        // 현재 난이도의 스폰 각격을 넘으면 스폰 호출
+        if (timer > spawnData[difficulty].spawnTime)
         {
             timer = 0;
             Spawn();
@@ -29,10 +41,13 @@ public class Spawner : MonoBehaviour
 
     void Spawn()
     {
-        // 풀에서 적을 꺼냄
-        GameObject enemy = GameManager.instance.pool.Get(Random.Range(0, GameManager.instance.pool.prefabs.Length));
+        // 현재 난이도 데이터가 지정한 종류(spriteType)의 프리펩을 풀에서 적을 꺼냄
+        GameObject enemy = GameManager.instance.pool.Get(spawnData[difficulty].spriteType);
         
         // 스폰 포인트 중 하나에 배치 (랜덤으로, point들에서만 시작되도록 0제외)
         enemy.transform.position = spawnPoint[Random.Range(1, spawnPoint.Length)].position;
+        
+        // 현재 난이도 데이터로 Enemy 능력치 초기화 (체력, 속도)
+        enemy.GetComponent<Enemy>().Init(spawnData[difficulty]);
     }
 }
