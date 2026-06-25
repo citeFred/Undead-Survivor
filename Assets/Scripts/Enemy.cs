4f96558009fe3180c5257c07478530ac0fd9e21c
citefred <citefred@gmail.com>
2026-06-25 12:22:47 +0900

    feat: Enemy 사망 후 FlipX 비활성화, 적 Layer Order 순서 변경

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -52,7 +52,7 @@ public class Enemy : MonoBehaviour
         // 사망에서 바뀌엇던 상태들 모두 원복(재사용위해서)
         coll.enabled = true;
         rigid.simulated = true;
-        spriter.sortingOrder = 2;
+        spriter.sortingOrder = 3;
         anim.SetBool("Dead", false);
     }
 
@@ -78,6 +78,9 @@ public class Enemy : MonoBehaviour
 
     void LateUpdate()
     {
+        if (!isLive)
+            return;
+        
         spriter.flipX = target.position.x < rigid.position.x;
     }
 
@@ -103,7 +106,7 @@ public class Enemy : MonoBehaviour
             isLive = false; // 시체의 이동,반응 정지(FixedUpdate 필터용)
             coll.enabled = false; // 시체의 충돌 제거
             rigid.simulated = false; // 물리 정지(밀리거 움직임 정지)
-            spriter.sortingOrder = 1; // 정렬을 내림
+            spriter.sortingOrder = 2; // 정렬을 내림
             anim.SetBool("Dead", true); // 사망 애니메이션 재생을 위한 파라미터 값 전달
 
         }
