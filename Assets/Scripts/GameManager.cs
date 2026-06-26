24512c4e9f4de5cd474a03962eaff774d626ba53
citefred <citefred@gmail.com>
2026-06-26 14:29:56 +0900

    fix: HUD의 무한 레벨업과 배열 넘침 예외에 대한 동적리스트 자료구조를 통한 변수 타입 변경

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -16,7 +17,7 @@ public class GameManager : MonoBehaviour
     public int level;
     public int kill;
     public int exp;
-    public int[] nextExp = { 3, 5, 10, 20, 150, 210, 280, 360, 450, 600 }; // 임시 레벨업 테이블
+    public List<int> nextExp = new List<int> { 3, 5, 10, 20, 150, 210, 280, 360, 450, 600 }; // 동적 배열을 위해 일반 배열 대신 리스트 자료구조 변경
     public int health;
     public int maxHealth = 100;
     
@@ -51,6 +52,12 @@ public class GameManager : MonoBehaviour
         {
             level++;
             exp = 0;
+
+            // 초기 레벨 이상 초과시, 경험치 테이블을 추가하면서 최대 경험치를 복사
+            if (level >= nextExp.Count)
+            {
+                nextExp.Add(nextExp[nextExp.Count - 1]);
+            }
         }
     }
 }
